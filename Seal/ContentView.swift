9610ec32e139edb05b9cbe614a3f505038ca42bb
Jason Page <72579902+jasonepage@users.noreply.github.com>
2026-06-11 17:52:48 -0700

    Initial Commit

---
+++ Seal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Seal
+//
+//  Created by Jason Page on 6/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Seal/SealApp.swift
@@ -0,0 +1,17 @@
+//
+//  SealApp.swift
+//  Seal
+//
+//  Created by Jason Page on 6/11/26.
+//
+
+import SwiftUI
+
+@main
+struct SealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
